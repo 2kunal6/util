0151605f414bf4e60be742506806c924d54b00fd
Kunal Rout <2kunal6@gmail.com>
2024-03-17 16:17:32 +0530

    docker compose with faiss and streamlit

---
+++ src/docker_compose/faiss/faiss_index.py
@@ -0,0 +1,17 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+class FAISSindex:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls.db = FAISS.load_local('faiss/faiss_index', HuggingFaceEmbeddings(),
+                                      allow_dangerous_deserialization=True)
+
+        return cls._instance
+
+    def search_similar_docs(self, query, n_results):
+        return self.db.similarity_search(query, n_results)
